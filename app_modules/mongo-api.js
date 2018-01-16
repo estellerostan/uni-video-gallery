db43bebc83d12f1b280ad80dadd7861899f5489c
Estelle Rostan <estelle.rostan@etu.unice.fr>
2018-01-16 16:38:51 +0100

    Refactor const vs let
    Utiliser const au lieu de let pour les variables qui contiennent des informations qui ne changeront jamais.

--- app_modules/mongo-api.js
+++ app_modules/mongo-api.js
@@ -1,7 +1,7 @@
-let MongoClient = require('mongodb').MongoClient;
-let assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-let url = 'mongodb://localhost:27017/mbds';
+const url = 'mongodb://localhost:27017/mbds';
 
 exports.mongoConnection = function(callback) {
     MongoClient.connect(url, function(err, db) {
