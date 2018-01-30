ee71a17c36fee49410002a191ed48e69b14b78fc
Estelle Rostan <estelle.rostan@etu.unice.fr>
2018-01-30 16:02:49 +0100

    Ajoute la modification d'une vidéo par le formulaire
    Si la vidéo à ajouter a la même URL qu'une vidéo dans la base de données, celle-ci est mise à jour avec le titre et la description entrés dans le formulaire.

--- app_modules/mongo-api.js
+++ app_modules/mongo-api.js
@@ -25,27 +25,60 @@ exports.createVideo = function(formData, callback) {
                 title: formData.title,
                 description: formData.description
             };
-            // console.dir(JSON.stringify(toInsert));
-            db.collection("videos")
-                .insertOne(toInsert, function (err, result) {
-                    let response;
 
-                    if (err) {
-                        response = {
-                            success: false,
-                            error: err,
-                            msg: "ProblÃƒÂ¨me Ãƒ  l'insertion"
-                        };
-                    } else {
-                        response = {
-                            success: true,
-                            result: result,
-                            error: null,
-                            msg: "Ajout rÃƒÂ©ussi " + result
-                        };
-                    }
-                    callback(response);
-                });
+            const collection = db.collection("videos");
+
+            // Find the video to save
+            collection.findOne({url: formData.url}, function(err, item) {
+                // assert.equal(null, err);
+                // assert.equal(null, item.url);
+
+                if (item == null) {
+                    // we don't have a result
+                    collection
+                        .insertOne(toInsert, function (err, result) {
+                            let response;
+
+                            if (err) {
+                                response = {
+                                    success: false,
+                                    error: err,
+                                    msg: "ProblÃƒÂ¨me Ãƒ  l'insertion"
+                                };
+                            } else {
+                                response = {
+                                    success: true,
+                                    result: result,
+                                    error: null,
+                                    msg: "Ajout rÃƒÂ©ussi " + result
+                                };
+                            }
+                            callback(response);
+                        });
+                } else {
+                    // we have a result
+                    collection
+                    .updateOne({url: formData.url}, toInsert, function (err, result) {
+                        let response;
+
+                        if (err) {
+                            response = {
+                                success: false,
+                                error: err,
+                                msg: "ProblÃƒÂ¨me Ãƒ  l'insertion"
+                            };
+                        } else {
+                            response = {
+                                success: true,
+                                result: result,
+                                error: null,
+                                msg: "Ajout rÃƒÂ©ussi " + result
+                            };
+                        }
+                        callback(response);
+                    });
+                }
+            });
         }
     });
 };
